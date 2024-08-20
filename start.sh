11a3de0e5cf943855fd8e5befb36d6b529677f29
arthurcadot <arthurkdo@gmail.com>
2024-08-20 19:04:32 +0200

    fin des préparatifs

--- start.sh
+++ start.sh
@@ -1,1 +1,9 @@
-sudo apt install gettext man-db procps psmisc nano tree bsdmainutils x11-apps wget
+#!/bin/bash
+
+# Ajouter l'alias au fichier .bashrc
+echo "alias gsh='bash ~/gsh/gsh.sh'" >> ~/.bashrc
+
+# Installer les paquets
+sudo apt update
+sudo apt install -y gettext man-db procps psmisc nano tree bsdmainutils x11-apps
+
